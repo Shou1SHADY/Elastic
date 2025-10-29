b695660592bd74200abc6cdb032a7290b6cfaab3
Shady Ossama <shadyosama658@gmail.com>
2025-10-29 21:25:59 +0000

    add a map in the contact page

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -204,6 +204,18 @@ export default function ContactPage() {
                                         </div>
                                     </div>
                                 </div>
+                                <div className="mt-8 rounded-lg overflow-hidden shadow-lg">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.206434460498!2d-73.988242084593!3d40.7588959793268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6434225%3A0x1ba9e7464161a87a!2sTimes%20Square!5e0!3m2!1sen!2sus!4v1628884224535!5m2!1sen!2sus"
+                                        width="100%"
+                                        height="300"
+                                        style={{ border: 0 }}
+                                        allowFullScreen={true}
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                        title="Office Location"
+                                    ></iframe>
+                                </div>
                             </div>
                         </div>
                     </div>
